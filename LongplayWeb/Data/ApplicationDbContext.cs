8cb9680fc64746ddf94d02ac9dace6aa7f2cec6f
zoran.milovanovic <zoran.milovanovic@akvelon.com>
2023-01-30 22:24:41 +0100

    Add ApplicationDbContext

---
+++ LongplayWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LongplayWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LongplayWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
