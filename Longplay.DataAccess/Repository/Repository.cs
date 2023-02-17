9a855256d7560ff09388c549920d5b9845bc75a9
Zoran <zoranmilovanovic656@gmail.com>
2023-02-17 13:42:34 +0100

    Add IRepository interface

---
+++ Longplay.DataAccess/Repository/Repository.cs
@@ -0,0 +1,38 @@
+﻿using Longplay.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longplay.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
