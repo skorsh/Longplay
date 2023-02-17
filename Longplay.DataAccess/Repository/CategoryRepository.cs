4c2fd02bf6ae2890fcae8cb15b90b3a3e34fd8dc
Zoran <zoranmilovanovic656@gmail.com>
2023-02-17 14:36:07 +0100

    Implement Save and Update methods in CategoryRepository

--- Longplay.DataAccess/Repository/CategoryRepository.cs
+++ Longplay.DataAccess/Repository/CategoryRepository.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Longplay.DataAccess.Repository.IRepository;
+using Longplay.Model;
 
 namespace Longplay.DataAccess.Repository
 {
-    public interface CategoryRepository : ICategoryRepository
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _context.Update(obj);
+        }
     }
 }
--- Longplay.DataAccess/Repository/Repository.cs
+++ Longplay.DataAccess/Repository/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using Longplay.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
